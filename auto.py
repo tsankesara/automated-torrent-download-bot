c085f5589125bc5a9dea2c59ad2326e52d6c9dc6
Teerth Sankesara <50574830+tsankesara@users.noreply.github.com>
2020-05-05 00:17:56 +0530

    Imported web automation as function

--- auto.py
+++ auto.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from qbittorrent import Client
 from selenium.common.exceptions import NoSuchElementException
-
+import OneThreeThreeSevenX as OTTSx
 
 qb = Client("http://127.0.0.1:8080/")
 qb.login("admin", "adminadmin")
@@ -33,50 +33,14 @@ if enter == 3:
 
 n = len(lst)
 
-tor_not_found = []
-
-i = 0
-j = 0
-
 Wish_quality = str(input("Do you wish to set quality? Type 'Yes' or 'No': "))
 
 if Wish_quality == "Yes":
 	quality = int(input("Enter your required quality in terms of 480, 720 and 1080 DO NOT Write 'p' next to it: "))
 if Wish_quality == "No":
 	quality = str("")
-while n > i:
-  torr_name = str(lst[j] + " " + str(quality))
-  print("Downloading: " + torr_name)
 
-  browser = webdriver.Chrome(ChromeDriverManager().install())
-  browser.get('https://1337x.to/')
-  print("Browser Stated")
-  torr_input = browser.find_element_by_xpath('//*[@id="autocomplete"]')
-  torr_input.send_keys(torr_name)
-  print("Torrent Name Entred")
-
-  click_search = browser.find_element_by_xpath('//*[@id="search-form"]/button')
-
-  click_search.click()
-  print("Element Searched")
-  try:
-      first_sel = browser.find_element_by_xpath('/html/body/main/div/div/div/div[3]/div[1]/table/tbody/tr[1]/td[1]/a[2]')
-      first_sel.click()
-      print("Torrent Found")
-  except NoSuchElementException:
-      print(torr_name + " Not found")
-      tor_not_found.append(torr_name)
-      j = j+1
-      browser.close()
-      continue
-
-  Murl = browser.find_element_by_xpath('/html/body/main/div/div/div/div[2]/div[1]/ul[1]/li[1]/a')
-  magnet_link = Murl.get_attribute("href")
-  print("Magnet link copied")
-  browser.close()
-  qb.download_from_link(magnet_link)
-  print("\n Downloading: " + torr_name )
-  i = i+1
-  j = j+1
+OTTSx.Fmodule(n, lst, quality) #FirstModule is 1337x.to
+	
 
 print("Torrent Not found are: \n" + (list(tor_not_found)))
